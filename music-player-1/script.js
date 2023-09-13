d7f372bb0a24111d588609e3b4134bba20d65f0d
namanbh31 <85625598+namanbh31@users.noreply.github.com>
2023-09-13 11:06:19 +0530

    Add files via upload

---
+++ music-player-1/script.js
@@ -0,0 +1,14 @@
+var x1 = document.getElementById("song-control-footer-id");
+x1.style.display = "none";
+function myFunction() {
+    var x = document.getElementById("song-control-footer-id");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    }
+    else if(x.style.display!="none"){
+      x.style.display = "block";
+    }
+    else {
+      x.style.display = "none";
+    }
+  }
